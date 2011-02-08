7430387d315090527b7c034fa72b7fdb1f162264
shaunsephton <shaun@praekeltconsulting.com>
2011-02-08 15:21:37 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='djanginxed',
-    version='0.0.1',
+    version='0.0.2',
     description='Django Nginx Memcached integration.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
